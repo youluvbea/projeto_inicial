19299e2ba67e32464c2b5c5755561357b8a54f0f
youluvbea <uluvnick@gmail.com>
2024-03-18 09:55:42 -0300

    formulario

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
 var botao = document.getElementById("botao")
 botao.addEventListener("click", alertar);
+
 var nome = document.getElementById("nome");
 
+var saida= document.getElementById("saida-de-dados");
+
+var email= document.getElementById("email");
+
+var cep= document.getElementById("cep");
+
+var telefone= document.getElementById("telefone");
+
+var logradouro= document.getElementById("log");
+
+var numero= document.getElementById("numero");
+
+var comp= document.getElementById("comp");
+
+var bairro= document.getElementById("bairro");
+
+var cidade= document.getElementById("cid");
+
+var estado= document.getElementById("estado");
+
+
+
 function alertar(){
-    alert(nome.value = " " + "voce clicou no botao!!");
+    //alert(nome.value = " " + "voce clicou no botao!!");
+    saida.innerText= "nome:" + " " + nome.value + 
+    "\n E-mail:" + " " + email.value,  
+    "\n CEP:" + " " + cep.value,
+    "\n Telefone:" + " " + telefone.value,  
+    "\n Logrdouro:" + " " + log.value, 
+    "\n Numero:" + " " + numero.value,
+    "\n Complemento:" + " " + comp.value, 
+    "\n Bairro:" + " " + bairro.value,
+    "\n Cidade:" + " " + cid.value, 
+    "\n Estado:" + " " + estado.value;
 }
